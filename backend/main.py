5d8484c8fff05ca7ad331e9418e08184e8d0d816
sujoy0908 <sujoysamanta0908@gmail.com>
2026-07-05 11:55:26 +0530

    Fix Netlify routing

--- backend/main.py
+++ backend/main.py
@@ -322,4 +322,6 @@ async def get_portfolio_summary():
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
